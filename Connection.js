888d9f45717f9889bd7572860730d57e75f53c45
Snehil <snehilsaxena2903@gmail.com>
2026-03-15 01:15:44 +0530

    Updated Connections.js2

--- Connection.js
+++ Connection.js
@@ -1,15 +1,18 @@
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
 dotenv.config();
+const mongoose = require("mongoose");
 
-const Connection = () => {
-  mongoose.connect(process.env.MONGO_URL, {
-  serverSelectionTimeoutMS: 5000
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
-
+const Connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000
+    });
+    console.log("Mongo URL:", process.env.MONGO_URL);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
 
-
-module.exports = {Connection}
+module.exports = {Connection};
